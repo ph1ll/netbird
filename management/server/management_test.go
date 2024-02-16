6bbf5a3a40f956276be4a61c5672e14ab983406d
Zoltan Papp <zoltan.pmail@gmail.com>
2024-02-16 14:16:45 +0100

    Fix mod interface

--- management/server/management_test.go
+++ management/server/management_test.go
@@ -448,11 +448,11 @@ var _ = Describe("Management service", func() {
 type MocIntegratedValidator struct {
 }
 
-func (MocIntegratedValidator) PreparePeer(*nbpeer.Peer, *account.ExtraSettings) *nbpeer.Peer {
+func (MocIntegratedValidator) PreparePeer(string, *nbpeer.Peer, []string, *account.ExtraSettings) *nbpeer.Peer {
 	return nil
 }
 
-func (MocIntegratedValidator) ValidatePeer(peer *nbpeer.Peer) (bool, error) {
+func (MocIntegratedValidator) ValidatePeer(string, *nbpeer.Peer, []string, []string) (bool, error) {
 	return true, nil
 }
 
